83b7dedc1eccbed8173516d5831a002a6c2f2248
Shindeyashtech <yashshinde.tech@gmail.com>
2024-06-09 21:29:37 +0530

    process on product

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  const {all_product}=useContext(ShopContext);
+  const {productID} = useParams();
+  const Product = all_product.find((e)=> e.id === Number(productID));
+   
   return (
     <div>
       
