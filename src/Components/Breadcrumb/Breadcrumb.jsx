a5bc5888ff4214e5130965150ef3f32df5e38e31
Yash shinde <yashshinde.tech@gmail.com>
2024-07-27 16:56:21 +0530

    Update Breadcrumb.jsx

--- src/Components/Breadcrumb/Breadcrumb.jsx
+++ src/Components/Breadcrumb/Breadcrumb.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import '../Breadcrumb/Breadcrumb.css'
-const Breadcrumb = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React from 'react';
+import './Breadcrumb.css';
+import arrow_icon from './breadcrum_arrow.png';
 
-export default Breadcrumb
+const Breadcrumb = ({ product }) => {
+  if (!product) {
+    console.error('Product prop is undefined or null');
+    return <div className="breadCrumb">Product not found</div>;
+  }
+
+  return (
+    <div className="breadCrumb">
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />
+      {product.category} <img src={arrow_icon} alt="" />{product.name}
+    </div>
+  );
+};
+
+export default Breadcrumb;
